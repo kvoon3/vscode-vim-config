5bc1337b3359e46380dad83b8d3c2e7e98e1af22
kvoon <kvoon@duck.com>
2025-07-20 20:15:12 +0800

    chore: update docs

--- update.mjs
+++ update.mjs
@@ -6,6 +6,6 @@ const today = new Date().toISOString()
 fs.writeFileSync('README.md', fs.readFileSync('README.md', 'utf-8')
   .replace(/```json([\s\S]*?)```/, () => {
     return `\`\`\`jsonc
-${JSON.stringify(keybindings.unshift(`// ${today}`), null, 2)}
+${JSON.stringify([`// ${today}`, ...keybindings], null, 2)}
 \`\`\``.trim()
   }), 'utf-8')
